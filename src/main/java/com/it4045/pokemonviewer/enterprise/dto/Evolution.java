1a383e489e07e6ee4d8728200c82dbef9b81bd29
Tristan Palmer <palmertt@mail.uc.edu>
2020-10-08 11:51:57 -0400

    Added GSON dependency, added @SerializedName above proper variables

--- src/main/java/com/it4045/pokemonviewer/enterprise/dto/Evolution.java
+++ src/main/java/com/it4045/pokemonviewer/enterprise/dto/Evolution.java
@@ -1,10 +1,13 @@
 package com.it4045.pokemonviewer.enterprise.dto;
 
+import com.google.gson.annotations.SerializedName;
 import lombok.Data;
 
 public @Data
 class Evolution {
 
+    @SerializedName("num")
     private String evolutionNumber;
+    @SerializedName("name")
     private String pokemonEvolutionName;
 }
